f27fecc15d04154a6ba00eca642b42e32f53b20c
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-03-14 20:22:45 +0200

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { nanoid } from 'nanoid'
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: ''
   }
 
@@ -39,7 +34,10 @@ export default class App extends Component {
     }
   }
   componentDidMount() {
-    this.setState({ contacts: JSON.parse(localStorage.getItem('contacts')) })
+    const localContacts = JSON.parse(localStorage.getItem('contacts'))
+    if (localContacts) {
+      this.setState({ contacts: localContacts })
+    }
   }
   componentDidUpdate(prevP, prevS) {
     console.log(prevS);
@@ -86,7 +84,7 @@ export default class App extends Component {
         <>
           <Filter onChange={this.handleFilterChange} />
           <ContactList
-            contacts={!filter.length ? contacts : this.filteredContacts()}
+            contacts={filter.length > 0 ? contacts : this.filteredContacts()}
             onDelete={this.handleContactDelete}
           />
         </>
